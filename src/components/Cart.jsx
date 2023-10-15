706a45fdafb5c202ee3fac844db4f91a30ac6397
Quang Le Ngoc Ky <quang.lengocky@nashtechglobal.com>
2023-10-15 22:23:12 +0700

    Add sponsor assets

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { shoppingCartSelector, identitySelector } from "../redux/selector";
 import { useDispatch, useSelector } from "react-redux";
 import useTable from "../hooks/useTable";
-import { Button,  Form, Input, InputNumber, Select, Table } from "antd";
+import { Button, Form, Input, InputNumber, Select, Table } from "antd";
 import { removeItem } from "../redux/shoppingCartSlice";
 import cityProvince from '../data/city-province.json';
+import CartSummary from "./CartSummary";
 
 function Cart(props) {
   const [form] = Form.useForm();
@@ -44,6 +45,11 @@ function Cart(props) {
         render: (price) => `$${price}`,
       },
       {
+        title: "Unit",
+        dataIndex: "unit",
+        key: "unit",
+      },
+      {
         title: "Action",
         dataIndex: "id",
         key: "id",
@@ -54,29 +60,33 @@ function Cart(props) {
     ],
     summary: (pageData) => {
       let totalPrice = 0;
-      pageData.forEach(({ price }) => {
+      let totalUnit = 0;
+      pageData.forEach(({ price, unit }) => {
         totalPrice += price;
+        totalUnit += unit;
       });
       return (
-        <Table.Summary.Row>
-          <Table.Summary.Cell index={0}>Total</Table.Summary.Cell>
-          <Table.Summary.Cell index={1}></Table.Summary.Cell>
-          <Table.Summary.Cell index={2}>${totalPrice}</Table.Summary.Cell>
-        </Table.Summary.Row>
+        <Table.Summary fixed>
+          <CartSummary totalUnits={totalUnit} totalPrice={totalPrice} />
+        </Table.Summary>
       );
     },
   });
   return (
     <div className="cart">
-      <div style={{ display: "flex" }}>
-        <div className="shipping-info" style={{ flex: 2 }}>
-          <Form
-            form={form}
-            name="basic"
-            labelCol={{ span: 6 }}
-            wrapperCol={{ span: 4 }}
-            onFinish={onSubmit}
-          >
+      <Form
+        form={form}
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 8 }}
+        onFinish={onSubmit}
+        initialValues={{
+          ...identityInfo
+        }}
+      >
+        <div style={{ display: "flex" }}>
+          <div className="shipping-info" style={{ flex: 2 }}>
+
             <Form.Item
               label="First name"
               name="firstName"
@@ -112,21 +122,48 @@ function Cart(props) {
               name="city"
               rules={[{ required: true, message: "Please input city!" }]}
             >
-              <Select allowClear size="small" style={{ width: 200 }} options={cityProvince.data.filter(x=>x.level === "City")} />
+              <Select allowClear size="small" style={{ width: 200 }} options={cityProvince.data.filter(x => x.level === "City")} />
             </Form.Item>
             <Form.Item
               label="Province"
               name="province"
               rules={[{ required: true, message: "Please input province!" }]}
             >
-              <Select allowClear size="small" style={{ width: 200 }} options={cityProvince.data.filter(x=>x.level === "Province")} />
+              <Select allowClear size="small" style={{ width: 200 }} options={cityProvince.data.filter(x => x.level === "Province")} />
             </Form.Item>
-          </Form>
+
+          </div>
+          <div className="items" style={{ flex: 3 }}>
+            {renderTable()}
+            <div className="payment-detail" style={{ marginTop: 10 }}>
+              <hr />
+              <h5>Payment info</h5>
+              <Form.Item
+                label="Card number"
+                name="cardNumber"
+                rules={[{ required: true, message: "Please input card number!" }]}
+              >
+                <InputNumber size="small" style={{ width: 200 }} />
+              </Form.Item>
+              <Form.Item
+                label="Expiry"
+                name="expiry"
+                rules={[{ required: true, message: "Please input card expiry info!" }]}
+              >
+                <InputNumber name="month" placeholder="MM" size="small" style={{ width: 50 }} min={1} max={12}/>
+                <InputNumber name="year" placeholder="YY" size="small" style={{ width: 50 }} min={1} max={99} />
+              </Form.Item>
+              <Form.Item
+                label="CVV"
+                name="cvv"
+                rules={[{ required: true, message: "Please input card cvv value!" }]}
+              >
+                <InputNumber size="small" style={{ width: 200 }} />
+              </Form.Item>
+            </div>
+          </div>
         </div>
-        <div className="items" style={{ flex: 3 }}>
-          {renderTable()}
-        </div>
-      </div>
+      </Form>
       <Button type="primary" onClick={onPay}>
         Pay
       </Button>
